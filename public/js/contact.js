11fddc87c6e24661fadb10472f380455c9145e46
liger02 <211b233@juetguna.in>
2023-11-26 16:10:13 +0530

    vdaef

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,10 +1,10 @@
 
 let formBtn = document.querySelector('.btn');
-let loader = document.querySelector('.loader');
+//let loader = document.querySelector('.loader');
 formBtn.addEventListener('click', ()=>{
-    let name = document.querySelector('.cname') ;
-    let email = document.querySelector('.cemail') ;
-    let textarea = document.querySelector('.ctextarea');
+    let name = document.querySelector('#cname') ;
+    let email = document.querySelector('#cemail') ;
+    let textarea = document.querySelector('#ctextarea');
     
     if(name.value.length<1){
         showFormError("Please enter your name");
@@ -25,3 +25,34 @@ formBtn.addEventListener('click', ()=>{
         })
     }
 })
+/*
+window.onload = () =>{
+    user = JSON.parse(sessionStorage.user || null);
+    if(user == null){
+        location.replace('/login');
+    }
+}
+let applyBtn = document.querySelector('.con-btn');
+
+applyBtn.addEventListener('click', ()=>{
+
+    let businessName = document.querySelector('#name').value;
+    let address = document.querySelector('#address').value;
+    let number = document.querySelector('#number').value;
+    let about = document.querySelector('#about').value;
+    
+    if(!businessName.length || !about.length || !address.length || number < 10 || !Number(number)){
+        showFormError('some information(s) is/are incorrect');
+    }
+    else{
+        //send data
+        loader.style.display = 'block';
+        sendData('/seller',{
+            name: businessName,
+            address: address,
+            about: about,
+            number: number,
+            email: JSON.parse(sessionStorage.user).email
+        })
+    }
+})*/
